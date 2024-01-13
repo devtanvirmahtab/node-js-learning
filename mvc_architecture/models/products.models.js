ec8934bf2a1397f08abe7e190d64d6c23ff98767
devtanvirmahtab <devtanvirtoha@gmail.com>
2024-01-13 20:12:03 +0600

    learn mvc

---
+++ mvc_architecture/models/products.models.js
@@ -0,0 +1,13 @@
+const products = [
+    {
+        name:"Jackets",
+        price: 250,
+    },
+    {
+        name: "Shoes",
+        price: 600,
+    },
+];
+
+
+module.exports = products;
---
+++ mvc_architecture/models/users.model.js
@@ -0,0 +1,13 @@
+const users = [
+    {
+        name:"Tanvir",
+        age: "25",
+    },
+    {
+        name: "Toha",
+        age: 26,
+    },
+];
+
+
+module.exports = users;
